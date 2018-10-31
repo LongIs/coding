9ce3fb6cbd0fc50b2cb100ba222079c583d006ee
LongIs <798252779@qq.com>
2018-10-31 11:36:04 +0800

    测试

--- src/main/java/com/loong/principle/openclose/Test.java
+++ src/main/java/com/loong/principle/openclose/Test.java
@@ -5,7 +5,7 @@ public class Test {
         ICourse iCourse = new JavaDiscountCourse(99,"语文",12.2);
         JavaDiscountCourse javaCourse = (JavaDiscountCourse)iCourse;
 
-
+        /**/
         System.out.println("课程名:"+javaCourse.getName()+",课程id:"+javaCourse.getId()+
                 ",课程原价:"+javaCourse.getPrice()+",课程打折价:"+javaCourse.getSellPrice());
     }
