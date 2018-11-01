c02946881d04ca3a8626d7c902e7416600543c85
LongIs <798252779@qq.com>
2018-11-01 23:21:21 +0800

    sss

---
+++ src/main/java/com/loong/principle/singleresponsibility/Enum/EnumInstance.java
@@ -0,0 +1,10 @@
+package com.loong.principle.singleresponsibility.Enum;
+
+public enum EnumInstance {
+    INSTANCE;
+
+
+    public static EnumInstance getInstance(){
+        return INSTANCE;
+    }
+}
